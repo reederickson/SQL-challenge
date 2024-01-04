29c2441615f9435f56edb03090f51fa045b69ea4
reederickson <reed.erickson19@gmail.com>
2024-01-04 15:32:05 -0800

    completed analysis in SQL pgadmin

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -6,7 +6,7 @@
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
     "emp_title_id" VARCHAR(10)   NOT NULL,
-    "birth_date" VARCHAR(8)   NOT NULL,
+    "birth_date" DATE,
     "first_name" VARCHAR(20)   NOT NULL,
     "last_name" VARCHAR(20)   NOT NULL,
     "sex" VARCHAR(1)   NOT NULL,
@@ -15,6 +15,7 @@ CREATE TABLE "employees" (
         "emp_no"
      )
 );
+ALTER TABLE "employees" ALTER COLUMN "hire_date" TYPE DATE USING TO_DATE("hire_date", 'MM/DD/YYYY');
 
 CREATE TABLE "salaries" (
     "emp_no" INTEGER   NOT NULL,
@@ -43,7 +44,7 @@ CREATE TABLE "dept_emp" (
 );
 
 CREATE TABLE "dept_managers" (
-    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_no" VARCHAR(24)   NOT NULL,
     "emp_no" INTEGER   NOT NULL
 );
 
@@ -65,3 +66,72 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_managers;
+
+
+--Data Analysis
+--List the employee number, last name, first name, sex, and salary of each employee.
+SELECT employees.emp_no, employees.last_name, employees.first_name,employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no=salaries.emp_no;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1998' AND '12/31/1998'
+ORDER BY hire_date;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT departments.dept_no, departments.dept_name, dept_managers.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_managers
+ON departments.dept_no = dept_managers.dept_no
+JOIN employees
+ON dept_managers.emp_no = employees.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name= 'Hercules'
+AND last_name LIKE 'B%';
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT departments.dept_name,employees.emp_no, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name= 'Sales';
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name= 'Sales'
+OR departments.dept_name = 'Development';
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name,
+COUNT (last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
